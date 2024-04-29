21f37cc4b39b36613830fe99df4b1629e57d4719
kolabor11 <pkolabor11#gmail.com>
2024-04-29 19:23:14 +0200

    Log file parsere setup listing times and fails in the log file and comparing them with other log files

---
+++ CLIApp.cs
@@ -0,0 +1,30 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+
+class LogFileParser
+{
+    public Dictionary<string, (List<int> times, int fails)> ParseLogFile(string logFilePath)
+    {
+        var sessionData = new Dictionary<string, (List<int> times, int fails)>();
+        string sessionName = Path.GetFileNameWithoutExtension(logFilePath);
+        List<int> times = new List<int>();
+        int fails = 0;
+
+        foreach (string line in File.ReadLines(logFilePath))
+        {
+            if (line.StartsWith("[TIME]"))
+            {
+                times.Add(int.Parse(line.Split(": ")[1]));
+            }
+            else if (line.StartsWith("[FAIL]"))
+            {
+                fails++;
+            }
+        }
+
+        sessionData[sessionName] = (times, fails);
+        return sessionData;
+    }
+}
